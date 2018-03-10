72f878878412a113200b383e94458e97488ffd88
Vitaly Homonov <avg.idm@gmail.com>
2018-03-10 15:08:58 +0200

    fix winner determining logic

--- src/gameService.js
+++ src/gameService.js
@@ -1,9 +1,50 @@
+export const doesWinByVerticalLine = (symbol, lineIndex, board) => {
+  return board.every(row => row[lineIndex] === symbol);
+};
+
+export const doesWinByHorizontalLine = (symbol, lineIndex, board) => {
+  return board[lineIndex].every(s => s === symbol);
+};
+
+export const doesWinByDiagonalLine = (symbol, board) => {
+  const leftTopToBottomRightIndexes = [[0, 0], [1, 1], [2, 2]];
+  const rightTopToBottomLeftIndexes = [[0, 2], [1, 1], [2, 0]];
+
+  return [
+    leftTopToBottomRightIndexes,
+    rightTopToBottomLeftIndexes
+  ].some(indexes =>
+    indexes.every(([ rowIndex, columnIndex ]) => board[rowIndex][columnIndex] === symbol)
+  );
+};
+
+const isWin = (symbol, board) => {
+  let win = false;
+
+  [0, 1, 2].forEach(rowIndex => {
+    if (doesWinByHorizontalLine(symbol, rowIndex, board)) {
+      win = true;
+    }
+  });
+
+  [0, 1, 2].forEach(columnIndex => {
+    if (doesWinByVerticalLine(symbol, columnIndex, board)) {
+      win = true;
+    }
+  });
+
+  if (doesWinByDiagonalLine(symbol, board)) {
+    win = true;
+  }
+
+  return win;
+};
+
 export const gameStatus = board => {
-  const isWin = symbol => board[0].every(cell => cell === symbol);
-  if (isWin('X')) {
+  if (isWin('X', board)) {
     return 'X';
   }
-  if (isWin('O')) {
+  if (isWin('O', board)) {
     return 'O';
   }
 };
--- src/gameService.unit.js
+++ src/gameService.unit.js
@@ -1,6 +1,97 @@
-const { gameStatus } = require('./gameService');
+const { gameStatus, doesWinByVerticalLine, doesWinByHorizontalLine, doesWinByDiagonalLine } = require('./gameService');
 
-test('X should win', () => {
-  const board = [['X', 'X', 'X'], ['', '', ''], ['', '', '']];
-  expect(gameStatus(board)).toBe('X');
+describe('game service', () => {
+
+  describe('doesWinByVerticalLine', () => {
+    it('should be truth if board has selected vertical line filled with symbol', () => {
+      const board = [
+        ['X', '', ''],
+        ['X', 'X', ''],
+        ['X', '', 'X']
+      ];
+
+      expect(doesWinByVerticalLine('X', 0, board)).toBeTruthy();
+    });
+
+    it('should be falsy if board has not selected vertical line filled with symbol', () => {
+      const board = [
+        ['X', '', ''],
+        ['X', 'X', ''],
+        ['X', '', 'X']
+      ];
+
+      expect(doesWinByVerticalLine('X', 1, board)).toBeFalsy();
+  });
+});
+
+describe('doesWinByHorizontalLine', () => {
+  it('should be truth if board has selected horizontal line filled with symbol', () => {
+    const board = [
+      ['X', '', ''],
+      ['X', 'X', 'X'],
+      ['X', '', 'X']
+    ];
+
+    expect(doesWinByHorizontalLine('X', 1, board)).toBeTruthy();
+  });
+
+  it('should be falsy if board has not selected horizontal line filled with symbol', () => {
+    const board = [
+      ['X', '', ''],
+      ['X', 'X', ''],
+      ['X', '', 'X']
+    ];
+
+    expect(doesWinByHorizontalLine('X', 1, board)).toBeFalsy();
+});
+});
+
+describe('doesWinByDiagonalLine', () => {
+  it('should be truth if board has diagonal filled with symbol', () => {
+    const board = [
+      ['X', '', ''],
+      ['X', 'X', ''],
+      ['X', '', 'X']
+    ];
+
+    expect(doesWinByDiagonalLine('X', board)).toBeTruthy();
+  });
+
+  it('should be falsy if board has no diagonal filled with symbol', () => {
+    const board = [
+      ['X', '', ''],
+      ['X', 'X', ''],
+      ['X', '', '']
+    ];
+
+    expect(doesWinByDiagonalLine('X', board)).toBeFalsy();
+});
+});
+
+  describe('game status', () => {
+    it('should handle horizontal line', () => {
+      const board = [['X', 'X', 'X'], ['', '', ''], ['', '', '']];
+      expect(gameStatus(board)).toBe('X');
+    });
+
+    it('should handle diagonal line', () => {
+      const board = [
+        ['X', '', ''],
+        ['', 'X', ''],
+        ['', '', 'X']
+      ];
+
+      expect(gameStatus(board)).toBe('X');
+    });
+
+    it('should handle vertical line', () => {
+      const board = [
+        ['', '', 'X'],
+        ['', '', 'X'],
+        ['', '', 'X']
+      ];
+
+      expect(gameStatus(board)).toBe('X');
+    });
+  });
 });
